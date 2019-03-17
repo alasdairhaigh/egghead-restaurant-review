db5a7d677482c03e364e229f1de0dcb3a84e46d0
Alasdair Haigh <alasdair.haigh@gmail.com>
2019-03-17 15:53:59 +0000

    First commit: first 3 lessons of tutorial - create react native app; reload simulators; display and format text

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={{
+          padding:40,
+          fontSize:30,
+          textAlign:'center',
+          color: '#0066CC',
+          fontWeight: '300'
+        }}>Restaurant Review</Text>
+
+        <Text>React Cafe</Text>
+        <Text style={{color:'grey'}}>123 Anywhere St</Text>
+
+        <Text>Fancy Restaurant</Text>
+        <Text style={{color:'grey'}}>799 Main St</Text>
+      </View>
+    );
+  }
+}
